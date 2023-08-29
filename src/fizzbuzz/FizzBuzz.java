6cef5a57e41f0c24fe893b1717b6d632046a18f6
Emmanuelito18 <emmanuelito18yt@gmail.com>
2023-08-28 21:16:28 -0500

    FizzBuzz hecho en Java

--- src/fizzbuzz/FizzBuzz.java
+++ src/fizzbuzz/FizzBuzz.java
@@ -10,6 +10,19 @@ public class FizzBuzz {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here 
+        // TODO code application logic herefor(int i=1;i<=100;i++){
+        for(int i=1;i<=100;i++){
+            if(i%3==0&&i%5==0){
+                System.out.println("FizzBuzz");
+            }else{
+                if(i%3==0){
+                    System.out.println("Fizz");
+                }else if(i%5==0){
+                    System.out.println("Buzz");
+                }else{
+                    System.out.println(i);
+                }
+            }
+        }
     }
 }
