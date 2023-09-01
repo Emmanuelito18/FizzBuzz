746b90b2d51a4d5a0b5c8244ff4076aa9f6a8dd0
Emmanuelito18 <emmanuelito18yt@gmail.com>
2023-08-31 19:16:14 -0600

    se añadio un menu y código que calcula el modulo manualmente

--- recursos/main.c
+++ recursos/main.c
@@ -12,20 +12,6 @@ void FizzBuzzManual(void);
 void menu(void);
 
 int main(int argc, char *argv[]) {
-	/*int i=1;
-	for(i;i<=100;i++){
-		if(i%3==0&&i%5==0){
-			printf("FizzBuzz\n");
-		}else{
-			if(i%3==0){
-				printf("Fizz\n");
-			}else if(i%5==0){
-				printf("Buzz\n");
-			}else{
-				printf("%d\n",i);
-			}
-		}
-	}*/
 	menu();
 	return 0;
 	system("pause");
