209daaba5caa61c85efbc9b82e484724b1cc3194
Emmanuelito18 <emmanuelito18yt@gmail.com>
2023-08-31 19:08:23 -0500

    Se añadió codigo que comprueba cociciente manualmente

--- src/fizzbuzz/FizzBuzz.java
+++ src/fizzbuzz/FizzBuzz.java
@@ -13,16 +13,33 @@ public class FizzBuzz {
         // TODO code application logic herefor(int i=1;i<=100;i++){
         for(int i=1;i<=100;i++){
             if(i%3==0&&i%5==0){
+            System.out.println("FizzBuzz");
+            }else{
+            if(i%3==0){
+            System.out.println("Fizz");
+            }else if(i%5==0){
+            System.out.println("Buzz");
+            }else{
+            System.out.println(i);
+            }
+            }
+            /*if(moduloManual(i,3)&&moduloManual(i,5)){
                 System.out.println("FizzBuzz");
             }else{
-                if(i%3==0){
+                if(moduloManual(i,3)){
                     System.out.println("Fizz");
-                }else if(i%5==0){
+                }else if(moduloManual(i,5)){
                     System.out.println("Buzz");
                 }else{
                     System.out.println(i);
                 }
-            }
+            }*/
         }
+    }
+    static boolean moduloManual(int dividendo,int divisor){
+        int resultado=0,cociente=0;
+        resultado=dividendo/divisor;
+        cociente=dividendo-(resultado*divisor);
+        return cociente==0;
     }
 }
